11c7db218249cfc828b041002f8c61abb0366958
Pranav Barbade <pranavbarbade20@gmail.com>
2026-03-16 23:11:42 +0530

    Final version: Lowered history title, increased fonts throughout

---
+++ app/src/main/java/com/pranavbarbade/pomodorotimer/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.pranavbarbade.pomodorotimer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
